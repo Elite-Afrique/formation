5fb12bf237ee392921074e21d1e56b1fb61d4d3d
DAO Hamadou <daohamadou@gmail.com>
2023-12-03 18:34:00 +0000

    -

--- frontend/src/environments/environment.prod.ts
+++ frontend/src/environments/environment.prod.ts
@@ -1,6 +1,6 @@
 
 const adresse = location.origin + '/backend/public/index.php/';
-const url: string = location.origin.includes('formation.elite-afrique.com')? adresse: 'http://127.0.0.1:8000/';
+const url: string = location.origin.includes('formation.elite-afrique.com')? adresse: 'http://localhost:8000/';
 export const environment = {
     production: true,
     urlApi: url
